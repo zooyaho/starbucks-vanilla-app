07a3092a4c499014d7a2401b9887463e6addba52
zooyaho <84559229+zooyaho@users.noreply.github.com>
2022-03-20 20:46:44 +0900

    요소 슬라이드 구현 완료

--- js/main.js
+++ js/main.js
@@ -49,3 +49,47 @@ fadeEls.forEach( function(fadeEl, index) {
   });
 
 });
+
+/* Notice-line Swiper */
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부 
+});
+
+/* Promotion Swiper */
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000 // default 3000(3초)
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+/*  토글버튼으로 프로모션 숨김 동작 구현 */
+const promotionEl = document.querySelector('.promotion');
+const poromotionTogglebtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+poromotionTogglebtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+
+});
