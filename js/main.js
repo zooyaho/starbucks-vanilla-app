2bcd5ccc99d5c7bac41c73a069cecd1c02eefe9a
zooyaho <jiwoo1268@naver.com>
2022-03-22 23:46:59 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-// focus해재 시 실행 됨
-searchInputEl.addEventListener('blur', function() { 
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -163,8 +145,3 @@ spyEls.forEach( function (spyEl) {
     .addTo(new ScrollMagic.Controller()); // 우리가 추가한 옵션들을 내부 컨트롤러에 내용을 할당해서 실제로 동작할 수 있는 구조를 만들어주는 용도
 
 });
-
-/* Copyright 연도 자동 갱신 */
-const thisYear = document.querySelector('.this-year');
-
-thisYear.textContent = new Date().getFullYear(); // textContent: 요소의 글자 내용을 알아내거나 값을 지정하는 용도로 사용
