b6cafa13ca0fc4a01d9f7e28760006d3234c65a0
zooyaho <jiwoo1268@naver.com>
2022-04-19 20:26:41 +0900

    Moblie 서브 메뉴 활성화/비활성화 구현 완료

--- js/responsive.js
+++ js/responsive.js
@@ -40,54 +40,49 @@ menuClose.addEventListener('click', function(){
   });
 });
 
-/* 메뉴 리스트 펼침 */
+/* 메뉴 리스트 */
 const $toggleBtns = document.querySelectorAll('.gnb-menu .toggle-list');
-const $deepToggleBtns = document.querySelectorAll('.gnb-menu .deep-toggle-list');
 
-// 토글 버튼 리스트 이벤트함수
-function subListOpen({target}) {
-  console.log('현재 노드 :' ,target);
-  console.log('현재 노드의 .toggle-list 존재 유무 : ' + target.classList.contains('toggle-list'));
-  console.log('현재노드의 자식노드 존재 유무:' ,target.hasChildNodes());
-  console.log('arrow 버튼노드:' ,target.children[1]);
-  console.log('형제노드 :' ,target.nextElementSibling);
-
-  // if(!target.classList.contains('toggle-list')){}
-  if(target.nextElementSibling.style.display !== 'block'){
-    // sub-list 열림
-    target.nextElementSibling.style.display = 'block';
-    
-    // arrow btn 위로 회전
-    gsap.to(target.children[1], .4, {
-      transform: 'rotateX(-180deg)',
-    });
-  } else {
-    // sub-list 닫힘
-    target.nextElementSibling.style.display = 'none';
-    console.log('!!!!!!display: none!!!!!')
-    // arrow btn 아래로 회전
-    gsap.to(target.children[1], .4, {
-      transform: 'rotateX(0deg)',
-    });
-  }
-  
-  // 4/19: 이벤트 전파 오류 해결해야함
-  // target.stopPropagation();
-
-  console.log('!!!success!!!');
-}
-
-
-// 얕은 토글 버튼 리스트 이벤트부여
 [...$toggleBtns].forEach(function(toggleBtn){
-  console.log(toggleBtn);
-  toggleBtn.onclick = subListOpen;
-});
+  console.log('toggle-list 요소: ',toggleBtn);
 
-// 깊은 토글 버튼 리스트 이벤트부여
-[...$deepToggleBtns].forEach(function(toggleBtn){
-  console.log(toggleBtn);
-  toggleBtn.onclick = subListOpen;
+  // click 이벤트 부여
+  toggleBtn.addEventListener('click',function(e){
+    
+    let $parent = e.target.parentNode; // 클릭 범위에 해당하는 노드
+
+    // console.log('target: ',e.target);
+    // console.log('target 부모요소: ',e.target.parentNode);
+    // console.log('target 자식요소: ',$parent.children);
+    // console.log('arrow 버튼 요소: ',$parent.children[1]);
+
+    // 서브 메뉴 리스트가 있으면 실행
+    if($parent.nextElementSibling){
+      
+      if($parent.nextElementSibling.style.display !== 'block'){
+  
+        // sub-list 열림
+        $parent.nextElementSibling.style.display = 'block';
+  
+        // arrow btn 위로 회전
+        gsap.to($parent.children[1], .4, {
+          transform: 'rotateX(-180deg)',
+        });
+  
+      } else {
+  
+        // sub-list 닫힘
+        $parent.nextElementSibling.style.display = 'none';
+  
+        // arrow btn 아래로 회전
+        gsap.to($parent.children[1], .4, {
+          transform: 'rotateX(0deg)',
+        });
+  
+      }
+    }
+
+  });
 });
 
 
